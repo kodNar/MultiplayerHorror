0c5275d87d50d5aed6bfcac1734b8d36cbbc077a
kodNar <einaredberg@gmail.com>
2019-11-05 16:59:42 +0100

    Raytracing

---
+++ Multiplayer Horror/Assets/Scripts/CameraScripts/PlayerRaycasting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaycasting : MonoBehaviour
+{
+    public float distanceToSee;
+
+    private RaycastHit whatItHit; // se till att ta bort efter viss tid.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distanceToSee, Color.red);
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out whatItHit, distanceToSee)) //tar collidern för förmålet vår raycast träffade och lagrar den i "WhatItHit" variabeln.
+        {
+            Debug.Log("I touched "+whatItHit.collider.gameObject.name);
+            Destroy(whatItHit.collider.gameObject);
+        }
+    }
+}
