c951b655474b62d84c2f009bb91007fd2346137f
kodNar <einaredberg@gmail.com>
2019-11-15 22:16:12 +0100

    Alla knappar har animation

--- Multiplayer Horror/Assets/Scripts/Buttons/room1/ButtonScript.cs
+++ Multiplayer Horror/Assets/Scripts/Buttons/room1/ButtonScript.cs
@@ -7,13 +7,11 @@ using UnityEngine;
 public class ButtonScript : MonoBehaviour
 
 {
-    public GameObject definedButton;
     private new Animation animation;
 
     // Start is called before the first frame update
     void Start()
     {
-        definedButton = this.gameObject;
         animation = GetComponent<Animation>();
     }
 
@@ -28,28 +26,35 @@ public class ButtonScript : MonoBehaviour
             {
                 case "ButtonOne":
                     Debug.Log("Button 1");
-                    animation.Play("buttonAnimation");
+                    animation.Play("buttonAnimation1");
                     break;
                 case "ButtonTwo":
                     Debug.Log("Button 2");
+                    animation.Play("buttonAnimation2");
                     break;
                 case "ButtonThree":
                     Debug.Log("Button 3");
+                    animation.Play("buttonAnimation3");
                     break;
                 case "ButtonFour":
                     Debug.Log("Button 4");
+                    animation.Play("buttonAnimation4");
                     break;
                 case "ButtonFive":
                     Debug.Log("Button 5");
+                    animation.Play("buttonAnimation5");
                     break;
                 case "ButtonSix":
                     Debug.Log("Button 6");
+                    animation.Play("buttonAnimation6");
                     break;
                 case "ButtonSeven":
                     Debug.Log("Button 7");
+                    animation.Play("buttonAnimation7");
                     break;
                 case "ButtonEight":
                     Debug.Log("Button 8");
+                    animation.Play("buttonAnimation8");
                     break;
             }
         }
