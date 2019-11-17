61a8369e4fa508196b28f5001de451c6713a23b9
kodNar <einaredberg@gmail.com>
2019-11-17 13:34:29 +0100

    Knappar fungerar som de ska

--- Multiplayer Horror/Assets/Scripts/Room1/Buttons/room1/ButtonScript.cs
+++ Multiplayer Horror/Assets/Scripts/Room1/Buttons/room1/ButtonScript.cs
@@ -7,70 +7,76 @@ using UnityEngine;
 public class ButtonScript : MonoBehaviour
 
 {
-    private new Animation animation;
+    private static Queue<int> selectedColors = new Queue<int>();
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+    public GameObject button6;
+    public GameObject button7;
+    public GameObject button8;
 
     // Start is called before the first frame update
     void Start()
     {
-        animation = GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetMouseButtonDown(0))
         {
-            
             String buttonName = PlayerRaycasting.whatItHit.collider.gameObject.name;
 
             switch (buttonName)
             {
                 case "ButtonOne":
                     Debug.Log("Button 1");
-                    animation.Play("buttonAnimation1");
-                    ControlPanel.selectedColors.Enqueue(1);
+                    button1.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(1);
                     DeQueueColor();
                     break;
                 case "ButtonTwo":
                     Debug.Log("Button 2");
-                    animation.Play("buttonAnimation2");
-                    ControlPanel.selectedColors.Enqueue(2);
+                    button2.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(2);
                     DeQueueColor();
                     break;
                 case "ButtonThree":
                     Debug.Log("Button 3");
-                    animation.Play("buttonAnimation3");
-                    ControlPanel.selectedColors.Enqueue(3);
+                    button3.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(3);
                     DeQueueColor();
                     break;
                 case "ButtonFour":
                     Debug.Log("Button 4");
-                    animation.Play("buttonAnimation4");
-                    ControlPanel.selectedColors.Enqueue(4);
+                    button4.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(4);
                     DeQueueColor();
                     break;
                 case "ButtonFive":
                     Debug.Log("Button 5");
-                    animation.Play("buttonAnimation5");
-                    ControlPanel.selectedColors.Enqueue(5);
+                    button5.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(5);
                     DeQueueColor();
                     break;
                 case "ButtonSix":
                     Debug.Log("Button 6");
-                    animation.Play("buttonAnimation6");
-                    ControlPanel.selectedColors.Enqueue(6);
+                    button6.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(6);
                     DeQueueColor();
                     break;
                 case "ButtonSeven":
                     Debug.Log("Button 7");
-                    animation.Play("buttonAnimation7");
-                    ControlPanel.selectedColors.Enqueue(7);
+                    button7.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(7);
                     DeQueueColor();
                     break;
                 case "ButtonEight":
                     Debug.Log("Button 8");
-                    animation.Play("buttonAnimation8");
-                    ControlPanel.selectedColors.Enqueue(8);
+                    button8.GetComponent<Animation>().Play();
+                    selectedColors.Enqueue(8);
                     DeQueueColor();
                     break;
             }
@@ -78,19 +84,19 @@ public class ButtonScript : MonoBehaviour
 
         if (Input.GetKey(KeyCode.P))
         {
-            foreach (int ele in ControlPanel.selectedColors)
+            foreach (int ele in selectedColors)
             {
-               Debug.Log(ele.ToString());
+                Debug.Log(ele.ToString());
+                Debug.Log(selectedColors.Count);
             }
         }
     }
 
     private void DeQueueColor()
     {
-        if (ControlPanel.selectedColors.Count > 2)
+        if (selectedColors.Count > 2)
         {
-            ControlPanel.selectedColors.Dequeue();
+            selectedColors.Dequeue();
         }
-        
     }
 }
