40a1807d676f9840c6ef5f80b36dda68f6a278a5
kodNar <einaredberg@gmail.com>
2019-12-08 19:12:59 +0100

    Going good

--- Multiplayer Horror/Assets/Scripts/Room2/ButtonScriptR2.cs
+++ Multiplayer Horror/Assets/Scripts/Room2/ButtonScriptR2.cs
@@ -10,11 +10,27 @@ public class ButtonScriptR2 : MonoBehaviourPunCallbacks, IPunObservable
     public GameObject button3;
     public GameObject button4;
     public GameObject elevator1;
-
+    public GameObject door;
     private PhotonView PV;
+
+    private Animation animationOne;
+    private Animation animationTwo;
+    private Animation animationThree;
+    private Animation animationFour;
+    private Animation animationElevator;
+    private Animation animationDoor;
+
+
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animationOne = button1.GetComponent<Animation>();
+        animationTwo = button2.GetComponent<Animation>();
+        animationThree = button3.GetComponent<Animation>();
+        animationFour = button4.GetComponent<Animation>();
+        animationElevator = elevator1.GetComponent<Animation>();
+        animationDoor = door.GetComponent<Animation>();
         PV.GetComponent<PhotonView>();
     }
 
@@ -41,8 +57,32 @@ public class ButtonScriptR2 : MonoBehaviourPunCallbacks, IPunObservable
         PV.RPC("Button_Pressed_Network_R2", RpcTarget.All, hit);
     }
 
-    void Button_Pressed_Network_R2()
+    void Button_Pressed_Network_R2(string hit)
     {
-        
+        switch (hit)
+        {
+            case "ButtonOne":
+                Debug.Log("Button one");
+                animationOne.Play();
+                break;
+            case "ButtonTwo":
+                Debug.Log("Button Two");
+                animationTwo.Play();
+                break;
+            case "ButtonThree":
+                Debug.Log("Button three");
+                animationThree.Play();
+                break;
+            case "ButtonFour":
+                animationFour.Play();
+                
+                Debug.Log("Button four");
+                
+                break;
+            case "ButtonElevator":
+                animationElevator.Play();
+                Debug.Log("Elevator");
+                break;
+        }
     }
 }
